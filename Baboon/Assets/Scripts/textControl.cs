28ca766d5dc8ad0cf4f770caf98d4cc59f1267cd
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2014-10-01 15:34:51 -0400

    Added  music file, main menu and fonts

---
+++ Baboon/Assets/Scripts/textControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textControl : MonoBehaviour {
+
+	public bool isQuit = false;
+	public int levelToBeLoaded;
+
+	void Start () {
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseEnter(){
+		//change the color of the text
+		renderer.material.color = Color.black;
+	}
+
+	void OnMouseExit(){
+		//change the color of the text
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseUp(){
+		if (isQuit) {
+			Application.Quit();
+		} else {
+			Application.LoadLevel(levelToBeLoaded);
+		}
+	}
+}
