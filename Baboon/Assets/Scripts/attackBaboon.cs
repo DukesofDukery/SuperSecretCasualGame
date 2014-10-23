6bb0573369a7ea95f6612a4b554d91fdadb476ce
wtshuman <wtshuman@gmail.com>
2014-10-23 18:59:15 -0400

    Revert "Attacks"
    This reverts commit 7c147cc7ee90b8860239a8bcd9b707ef5a9a2323.

--- Baboon/Assets/Scripts/attackBaboon.cs
+++ Baboon/Assets/Scripts/attackBaboon.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class attackBaboon : MonoBehaviour {
-	
+
 	public float speed;
 	public RaycastHit hit;
 	
@@ -14,33 +14,39 @@ public class attackBaboon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		
-		//UPPERCUT
-		if(Input.GetKeyDown(KeyCode.W) && Mathf.RoundToInt(rigidbody.velocity.y) == 0){
-			rigidbody.AddForce(Vector3.up*2200);
-			rigidbody.AddForce(Vector3.right*10);
+		if(transform.position.z != 0){
+			transform.position = new Vector3(0,0,0);
 			
-			if(Physics.Raycast(transform.position,Vector3.right,out hit,4f)){
-				
-				hit.transform.GetComponent<buildingHealth>().health = hit.transform.GetComponent<buildingHealth>().health-2;
-
-				print ("hit");
-
+		}
+		if(transform.position.y < -10){
+			transform.position = new Vector3(0,0,0);
+			
+		}
+		
+		print (Mathf.RoundToInt(rigidbody.velocity.y));
+		if(Input.GetKeyDown(KeyCode.W) && Mathf.RoundToInt(rigidbody.velocity.y) == 0 /*&& GetComponent<moveBaboon>().attackTimer == 0*/){
+			rigidbody.AddForce(Vector3.up*800);
+			rigidbody.AddForce(Vector3.right*10);
+			//GetComponent<moveBaboon>().attackTimer=GetComponent<moveBaboon>().attackCooldown;
+			if(Physics.Raycast(transform.position,Vector3.right*2,out hit,2)){
+				if(hit.collider.tag == "Building"){
+					hit.transform.GetComponent<buildingHealth>().health = hit.transform.GetComponent<buildingHealth>().health-5;
+					//break;
+				}
 			}
 			
 		}
-		//BUTT SLAM
-		if(Input.GetKeyDown(KeyCode.S) && rigidbody.velocity.y <= 0){
+		
+		if(Input.GetKeyDown(KeyCode.S) && rigidbody.velocity.y <= 0 /*&& GetComponent<moveBaboon>().attackTimer == 0*/){
 			
-			rigidbody.AddForce(Vector3.down*6000);
+			rigidbody.AddForce(Vector3.down*3000);
 			rigidbody.AddForce(Vector3.right*50);
-			
+			//GetComponent<moveBaboon>().attackTimer=GetComponent<moveBaboon>().attackCooldown;
 			if(Physics.Raycast(transform.position,Vector3.down*10,out hit,10)){
-				
-				hit.transform.GetComponent<buildingHealth>().health = hit.transform.GetComponent<buildingHealth>().health-4;
-				
-				
+				if(hit.collider.tag == "Building"){
+					hit.transform.GetComponent<buildingHealth>().health = hit.transform.GetComponent<buildingHealth>().health-7;
+					
+				}
 			}
 		}
 	}
